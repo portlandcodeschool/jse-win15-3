1377e84de535bbd730d4c09cb343ebfe18344cc5
AntonEmery <antonemery@gmail.com>
2015-01-22 21:14:18 -0800

    problem 2b a bit

---
+++ AntonEmery/pseudo-array-template.js
@@ -0,0 +1,15 @@
+var array = {length:0,  // element properties will be added as needed
+
+pop: function() { //changes array, returns ??
+	//remove last item in the object, return modified length
+},
+
+push: function(/*...*/) { //changes array, returns a number
+	//...
+},
+
+join: function(/*...*/) { //returns a string
+	//...
+},
+
+};
