5770bd3fc43acd694b8a9de0f5e74e81dd58bc70
AntonEmery <antonemery@gmail.com>
2015-01-25 15:23:04 -0800

    question 2a psuedo code

--- AntonEmery/question3AntonEmery.js
+++ AntonEmery/question3AntonEmery.js
@@ -11,3 +11,29 @@ function copy(obj) {
 	return (b.a);
 }
 
+var objectA = {a:1, b:2, c:4};
+var objectB = {a:1, b:3, c:5};
+						
+
+function equal(objA, objB) {
+
+}
+
+function similar(objA, objB) {
+	
+	
+}
+
+// Psuedo Code for function similar
+see if the two objects are the same length
+	if yes continue
+	if no return false
+loop grab each key in object obj A 
+ask whether the key is also in B 
+	if no return false
+	if yes keep going
+return true
+
+/*3b*/
+
+
