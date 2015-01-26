2d01284948cb385d87dd29c1895ebb6345371fcc
KyleHouston84 <chariot124@gmail.com>
2015-01-25 21:56:22 -0800

    Problem 4 fully tested and commented.

--- KyleHouston/Homework-wk3-social-network.js
+++ KyleHouston/Homework-wk3-social-network.js
@@ -5,64 +5,67 @@ people.index = {}; 	//each property here will be named after a person
 					//and hold a reference to an object representing that person.
 
 people.meet = function(nameA,nameB) { // returns a number
-	if (nameA == nameB) {
-		return "You cannot meet oneself!!!"
+	//Check to see if the user entered the same name twice
+	if (nameA == nameB) {				
+		return "One cannot meet oneself!!!"
 	} 
-	if (!people.inObj(nameA)) {
+	//Check to see if the names entered are already in the object, if not then create them
+	if (!people.present(nameA)) {
 		people.addName(nameA);
 	}
-	if (!people.inObj(nameB)) {
+	if (!people.present(nameB)) {
 		people.addName(nameB);
 	}
-	
-	if (!(nameB in people['index'][nameA]['friends'])){
-		people['index'][nameA]['friends'][nameB] = 1;
+	//Check if this is the first time these 2 people have met, set the 'met' counter to 1 if they haven't, 
+	//increment the counter if they have
+	if (!(nameB in people['index'][nameA]['friends'])){	
+		people['index'][nameA]['friends'][nameB] = 1;	
 	} else {
-		people['index'][nameA]['friends'][nameB] += 1;
+		people['index'][nameA]['friends'][nameB]++; 	
 	}
 
-	if (!(nameA in people['index'][nameB]['friends'])){
-		people['index'][nameB]['friends'][nameA] = 1;
+	if (!(nameA in people['index'][nameB]['friends'])){	
+		people['index'][nameB]['friends'][nameA] = 1;	
 	} else {
-		people['index'][nameB]['friends'][nameA] += 1;
+		people['index'][nameB]['friends'][nameA]++;		
 	}
 
-	return people['index'][nameA]['friends'][nameB];
+	return people['index'][nameA]['friends'][nameB]; 	//output the number of times these 2 have met
 }
 
 people.haveMet = function(nameA,nameB) { //returns a number or falsish
-	if ((!people.inObj(nameA)) || (!people.inObj(nameB))) {
-		return undefined;
-	}
-	if (!(nameA in people['index'][nameB]['friends'])){
+	//Return undefined if either name is not found in the object or the 2 names have yet to meet
+	if ((!people.present(nameA)) || (!people.present(nameB)) || (!(nameA in people['index'][nameB]['friends']))) {
 		return undefined;
 	} else {
-		return people['index'][nameB]['friends'][nameA];
+		return people['index'][nameB]['friends'][nameA]; //output the number of times the two people have met
 	}
 }
 
 people.friendsOf = function(name) { //returns a string
-	if (!people.inObj(name)) {
-		return undefined;
+	if (!people.present(name)) { 
+		return undefined;	//return undefined if the name is not found in the object
 	}
-	var friendsList = [];
+	var friendsList = []; //set an array to store the names from the object for sorting and output
 
 	for (var names in people['index'][name]['friends']) {
-		friendsList.push(names);
+		friendsList.push(names); //add all of the friends from this name to the array
 	}
 
-	friendsList.sort();
-	return friendsList.join(' ');
+	friendsList.sort();	
+	return friendsList.join(', '); //output the array as a string
 	
 }
 
 people.friendsOfFriendsOf = function(name) {
-	if (!people.inObj(name)) {
+	if (!people.present(name)) {
 		return undefined;
 	}
+	//call function people.friendsOf and capture it in an array
+	var friendsOfFriends = people.friendsOf(name).split(', ');
 
-	var friendsOfFriends = people.friendsOf(name).split(' ');
-
+	//loop through the object friends of this person's friends and add them to the array if they are not 
+	//already in it (or their own name!)
 	for (var key in people['index'][name]['friends']) {
 		for (var key2 in people['index'][key]['friends']) {
 			if ((friendsOfFriends.indexOf(key2) == -1) && (key2 !== name)){
@@ -72,14 +75,14 @@ people.friendsOfFriendsOf = function(name) {
 	}
 
 	friendsOfFriends.sort();
-	return friendsOfFriends.join(' ');
+	return friendsOfFriends.join(', ');
 }
 
-people.inObj = function(name){
-	return (name in people.index);
+people.present = function(name){  //returns true or false
+	return (name in people.index);  //look for this person in the object
 }
 
-people.addName = function(name) {
+people.addName = function(name) { //adds the unfound name to the object
 	people['index'][name] = {friends: {}};
 	people['index'][name][name] = 0;
 }
