d0939baf423f096fa18205e7bdafae081e606d1d
Robert Tribbia <rtribbia@gmail.com>
2015-01-26 15:40:34 -0800

    Added 3C

--- rtribbia/homework.js
+++ rtribbia/homework.js
@@ -1,9 +1,10 @@
-1. See cards2-template.js
+//1. See cards2-template.js
 
-2.A. See array-test-template.js
-  B.&C. See pseudo-array-template.js 
+//2.A. See array-test-template.js
+//B.&C. See pseudo-array-template.js 
 
-3.A. function copy(obj) {
+//3.A. -----------------------------------------
+	function copy(obj) {
 		var iso = (typeof obj == 'object');
 		var dupe = { };
 
@@ -50,7 +51,8 @@
 
 	}
 
-	3.B. function union(objA, objB) { 
+	//3.B. ------------------------
+	function union(objA, objB) { 
 			var iso = ((typeof objA == 'object') && (typeof objB == 'object'));
 			var new_obj = {};
 			if (iso) {
@@ -95,7 +97,32 @@
 			}
 		}
 
-	3.C.
-	3.D.
+	//3.C. ------------------
+	function expectValue(result, expected, attemptStr) {
+		if (!equal(result,expected)) {
+			console.log(attemptStr+" expected result "+expected+", got "+result);
+		}
+	}
+	var objA = {a:1,b:0};
+	var objB = {a:0,c:0};
+	var objC = {a: 2, b: 3, c:4 };
+	
+	//UNION
+	expectValue(union(objA,objB), {a:1,b:0,c:0}, 'union(objA,objB)');
+	expectValue(union(objB,objA), {a:1,b:0,c:0}, 'union(objB,objA)');
+	expectValue(union(objC,objA), {a: 2, b: 3, c:4 }, 'union(objC,objA)');
 
-	4. See social-network-template.js
+	//INTERSECTION
+	expectValue(intersection(objA,objB), {a:0}, 'intersection(objA,objB)');
+	expectValue(intersection(objB,objA), {a:0}, 'intersection(objB,objA)');
+	expectValue(intersection(objC,objA), {a: 1, b: 0}, 'intersection(objC,objA)');
+
+	//SUBTRACT
+	expectValue(subtract(objA,objB), {b:0}, 'subtract(objA,objB)');
+	expectValue(subtract(objB,objA), {c:0}, 'subtract(objB,objA)');
+	expectValue(subtract(objC,objA), {c:4}, 'subtract(objC,objA)');
+
+	//3.D.-------------------
+
+
+	//4. See social-network-template.js
