61c3ad26e1300bcb559848f391e973428f4374ad
Todd Redmond <tredmond@urbangalaxie.com>
2015-01-28 00:06:21 -0800

    #1, #2.a, 3a,3b,3d,4a complete, but still working on the others

--- toddredmond/hw3_todd.js
+++ toddredmond/hw3_todd.js
@@ -132,7 +132,138 @@ expectNaN	(cardTools.name(-1), "name(-1)");
 expectNaN	(cardTools.name(52), "name(52)");
 expectNaN	(cardTools.name(NaN), "name(NaN)");
 
+//2a *************** Start of #2a ***********************/
+// Here's a fancy new assertion, with better failure reporting:
+function expectValue(result, expected, attemptStr) {
+	if (result !== expected) {
+		console.log(attemptStr+" expected result "+expected+", got "+result);
+	}
+}
+// In your tests below, you may use either kind of assertion, or write your own!
 
+function testPush(array) { // accept an array to test, if provided
+	// otherwise make a new one:
+	if (!array) array = [];
+
+	array.length = 0; //clear the array to be empty initially
+
+	// Try several pushes, and for each try, check the status of
+	//  the resulting array and the return value:
+	expectValue(array.push('a'), 1, "array.push('a')"); //check return value
+	expectValue(array[0], 'a', "array[0]"); // check array element
+	expectValue(array.length, 1, "array.length");	// check array length
+
+	expectValue(array.push('b'), 2, "array.push('b')");
+	expectValue(array[0], 'a', "array[0]"); //should remain 'a'
+	expectValue(array[1], 'b', "array[1]");
+	expectValue(array.length, 2, "array.length");
+
+	// That might be enough, but to be sure, push 'c' and test again here:
+	expectValue(array.push('c'), 3, "array.push('c')");
+	expectValue(array[0], 'a', "array[0]"); //should remain 'a'
+	expectValue(array[1], 'b', "array[1]");
+    expectValue(array[2], 'c', "array[1]");
+	expectValue(array.length, 3, "array.length");
+  
+    console.log(array[0]);
+    console.log(array[1]);
+    console.log(array[2]);
+}
+	function testPop(array) {
+	if (!array) array = [];
+
+	array.length = 0; //clear array
+
+	// 1) populate the array by pushing elements 'a' and 'b' onto it
+	//expectValue(array.push('a'));
+	//expectValue(array.push('b'));
+  	expectValue(array.push('a'), 1, "array.push('a')"); //check return value
+	expectValue(array[0], 'a', "array[0]"); // check array element
+	expectValue(array.length, 1, "array.length");	// check array length
+
+	expectValue(array.push('b'), 2, "array.push('b')");
+	expectValue(array[0], 'a', "array[0]"); //should remain 'a'
+	expectValue(array[1], 'b', "array[1]");
+	expectValue(array.length, 2, "array.length");
+  
+	// 2) pop once, then check the return value, array contents, and array length
+	//expectValue(array.pop());
+  
+	expectValue(array.pop('b'), 'b', "array.pop");
+	expectValue(array[0], 'a', "array[1]"); //should remain 'a'
+	expectValue(array.length, 1, "array.length");
+
+
+	// 3) pop again, then check as before
+	//...
+	expectValue(array.pop('a'), 'a', "array.pop");
+	expectValue(array[], , "array[[]]"); //should remain 'a'
+  expectValue(array.length, 0, "array.length");
+	// 4) array should now be empty!  check an attempt to pop when empty
+	//...
+}
+function testPop(array) {
+	if (!array) array = [];
+
+	array.length = 0; //clear array
+
+	// 1) populate the array by pushing elements 'a' and 'b' onto it
+    array.push('a');
+	array.push('b');
+
+	// 2) pop once, then check the return value, array contents, and array length
+	//expectValue(array.pop());
+	expectValue(array.pop('b'), 'b', "array.pop('b')");
+	expectValue(array[0], 'a', "array[0]"); //should remain 'a'
+	expectValue(array.length, 1, "array.length");
+
+
+	// 3) pop again, then check as before
+	expectValue(array.pop('a'), 'a', "array.pop('a')");
+	expectValue(array[0], undefined, "array[]"); //should remain 'a'
+	expectValue(array.length, 0, "array.length");
+	// 4) array should now be empty!  check an attempt to pop when empty
+	expectValue(array.pop(),undefined, "array.pop()");
+}
+
+function testJoin(array) {
+	if (!array) array = [];
+
+	array.length = 0; //clear array
+
+	// 1) try a join on the empty array,
+	//  then check the return value and its status (which should be unchanged)
+	//... 
+		array.join();
+
+	// 2) push 'a', then join and check as before
+	//...
+		array.push('a');
+    	expectValue(array.join(' '), 'a', "array.join('a b c')");
+      console.log(array)
+	// 3) push 'b', then join and check as before
+    	array.push('b');
+    	expectValue(array.join(' '), 'a b', "array.join('a b c')");
+      console.log(array)
+	// 4) push 'c', then join and check as before
+    	array.push('c');
+    	expectValue(array.join(' '), 'a b c', "array.join('a b c')");
+      console.log(array)
+	// 5) leave array unchanged, but join it with a different delimiter and check outcome
+     	array.join('-');
+
+	// 6) leave array unchanged, but join() it with no delimiter argument, and check that it
+	//   uses the default delimiter ',' correctly
+     	array.join();
+}
+
+// When those test functions are written, you can run them on built-in arrays
+//  by calling the test with no arguments:
+testPush();
+testPop();
+testJoin();
+
+//2a *************** End of of #2a ***********************/
 //3a *************** Start of #3 ***********************/
 
 
@@ -272,14 +403,8 @@ function expectValue(result, expected, attemptStr) {
 	}
 }
 
-function testIntersect(objA, objB) { // accept an array to test, if provided
-	// otherwise make a new one:
-	if (!array) array = {};
+function testIntersect(objA, objB) { 
 
-	//array.length = 0; //clear the array to be empty initially
-
-	// Try several pushes, and for each try, check the status of
-	//  the resulting array and the return value:
 	expectValue(intersection({{a:2,h:4,J:7},{U:6,h:4,K:3}), {h:4}, "array.push('a')"); //check return value
 
 
@@ -294,5 +419,62 @@ be true, but `equal(union(A,B),union(B,A))` may not be.  Likewise with intersect
 The reason is that by switching which objct is first, specific operators will choose the first true value 
 and not consider the other objects matching key. First instance, if ObjA and ObjB both have a 'B' key, but ObjA
 is evaluated first, the value of the 'b' key of objA will be used no matter what value is contant in ObjB.B.
+/*********** End of 3d Question ***********************************/
 
+/***********Start of 4a Question***********************************/
+//4a
+
+people.index = {}; 	//each property here will be named after a person
+					//and hold a reference to an object representing that person.
+
+people.meet = function(nameA,nameB) { // returns a number
+//return total number of times met
+//if not in people, add
+//increment number of meetings
+//order of arguements does not matter
+	var objA = people.index[nameA];
+
+		if (!objA) { // objA is either undefined or some obj
+		  objA = {name:nameA, friends: {} }
+		  people.index[nameA] = objA;
+		  objA.friends[nameB] = 0;
+    } 
+
+	  objA.friends[nameB] += 1
+  
+  console.log(objA.friends);
+	return objA.friends[nameB];
+	};
+
+//people.meet('todd','sam');
+
+people.haveMet = function(nameA,nameB) { 
+  console.log(objA.friends[nameB])
+  if (people.index[nameB]&&people.index[nameA] > 0) {
+    console.log("this is working")
+  }
+  console.log("No bueno");
+
+}
+people.haveMet('todd','sam');
+
+
+
+people.friendsOf = function(name) { //returns a string
+	//...
+}
+
+
+people.haveMet(nameA,nameB) {
+	//return # > 0 if met
+	//otherwise fasleish value
+}
+	friendsOf(name) {
+	//return string listing names of all 
+	//people whom name has met at least one of undefined 
+	//if name doesn't exist.
+	//list names in alpha order
+	}
+	}
+}
 
